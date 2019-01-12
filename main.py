3f386be0527bf7b8bdc3f88298a9940eb0b03b70
CodeBag-Debashish <dekadebashish3@gmail.com>
2019-01-12 13:25:02 +0530

    Project Initialized with a main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from multiprocessing import Process
+import random
+import time
+
+def some_function(first, last):
+    time.sleep(random.randint(1, 10))
+    print(first, last)
+
+processes = []
+
+for m in range(1,16):
+   n = m + 1
+   p = Process(target=some_function, args=(m, n))
+   p.start()
+   print(p," starts");
+   processes.append(p)
+
+for p in processes:
+   p.join()
